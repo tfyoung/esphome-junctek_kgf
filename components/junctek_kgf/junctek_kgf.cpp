01709b3c4cc05a265ae35d74ad2e757db0df82a5
shiftybugger <110100341+shiftybugger@users.noreply.github.com>
2023-01-30 23:36:30 +1100

    Update junctek_kgf.cpp

--- components/junctek_kgf/junctek_kgf.cpp
+++ components/junctek_kgf/junctek_kgf.cpp
@@ -9,22 +9,22 @@ static const char *const TAG = "JunkTek KG-F";
 
 
 // Get a value where it's expected to be "<number>[,.], incrementing the cursor past the end"
-int getval(const char*& cursor)
-{
-  const char* pos = cursor;
-  char* end = nullptr;
-  const long val = strtoll(pos, &end, 10);
-  if (end == pos || end == nullptr)
-  {
-    throw std::runtime_error("invalid number");
-  }
-  if (*end != ',' && *end != '.')
-  {
-    throw std::runtime_error("no coma");
-  }
-  cursor = end + 1;
-  return val;
-}
+// int getval(const char*& cursor)
+// {
+//   const char* pos = cursor;
+//   char* end = nullptr;
+//   const long val = strtoll(pos, &end, 10);
+//   if (end == pos || end == nullptr)
+//   {
+//     throw std::runtime_error("invalid number");
+//   }
+//   if (*end != ',' && *end != '.')
+//   {
+//     throw std::runtime_error("no coma");
+//   }
+//   cursor = end + 1;
+//   return val;
+// }
 
 JuncTekKGF::JuncTekKGF(unsigned address)
   : address_(address)
@@ -37,138 +37,133 @@ void JuncTekKGF::dump_config()
   ESP_LOGCONFIG(TAG, "  address: %d", this->address_);
 }
 
-void JuncTekKGF::handle_settings(const char* buffer)
-{
-  const char* cursor = buffer;
-  const int address = getval(cursor);
-  if (address != this->address_)
-    return;
-  const int checksum = getval(cursor);
-  if (! verify_checksum(checksum, cursor))
-    return;
+// void JuncTekKGF::handle_settings(const char* buffer)
+// {
+//   const char* cursor = buffer;
+//   const int address = getval(cursor);
+//   if (address != this->address_)
+//     return;
+//   const int checksum = getval(cursor);
+//   if (! verify_checksum(checksum, cursor))
+//     return;
 
-  const float overVoltage = getval(cursor) / 100.0;
-  const float underVoltage = getval(cursor) / 100.0;
-  const float positiveOverCurrent = getval(cursor) / 1000.0;
-  const float negativeOverCurrent = getval(cursor) / 100.00;
-  const float overPowerProtection = getval(cursor) / 100.00;
-  const float overTemperature = getval(cursor) - 100.0;
-  const int protectionRecoverySeconds = getval(cursor);
-  const int delayTime = getval(cursor);
-  const float batteryAmpHourCapacity = getval(cursor) / 10.0;
-  const int voltageCalibration = getval(cursor);
-  const int currentCalibration = getval(cursor);
-  const float temperatureCalibration = getval(cursor) - 100.0;
-  const int reserved = getval(cursor);
-  const int relayNormallyOpen = getval(cursor);
-  const int currentratio = getval(cursor);
-  // NOTE these are in the docs, but I don't seem to get them
-  //const int voltageCurveScale = getval(cursor);
-  //const int currentCurveScale = getval(cursor);
+//   const float overVoltage = getval(cursor) / 100.0;
+//   const float underVoltage = getval(cursor) / 100.0;
+//   const float positiveOverCurrent = getval(cursor) / 1000.0;
+//   const float negativeOverCurrent = getval(cursor) / 100.00;
+//   const float overPowerProtection = getval(cursor) / 100.00;
+//   const float overTemperature = getval(cursor) - 100.0;
+//   const int protectionRecoverySeconds = getval(cursor);
+//   const int delayTime = getval(cursor);
+//   const float batteryAmpHourCapacity = getval(cursor) / 10.0;
+//   const int voltageCalibration = getval(cursor);
+//   const int currentCalibration = getval(cursor);
+//   const float temperatureCalibration = getval(cursor) - 100.0;
+//   const int reserved = getval(cursor);
+//   const int relayNormallyOpen = getval(cursor);
+//   const int currentratio = getval(cursor);
+//   // NOTE these are in the docs, but I don't seem to get them
+//   //const int voltageCurveScale = getval(cursor);
+//   //const int currentCurveScale = getval(cursor);
 
-  // Save the capacity for calculating the %
-  this->battery_capacity_ = batteryAmpHourCapacity;
+//   // Save the capacity for calculating the %
+//   this->battery_capacity_ = batteryAmpHourCapacity;
 
-  this->last_settings_ = millis();
-}
+//   this->last_settings_ = millis();
+// }
 
-void JuncTekKGF::handle_status(const char* buffer)
-{
-  const char* cursor = buffer;
-  const int address = getval(cursor);
-  if (address != this->address_)
-    return;
+// void JuncTekKGF::handle_status(const char* buffer)
+// {
+//   const char* cursor = buffer;
+//   const int address = getval(cursor);
+//   if (address != this->address_)
+//     return;
 
-  const int checksum = getval(cursor);
-  if (! verify_checksum(checksum, cursor))
-    return;
+//   const int checksum = getval(cursor);
+//   if (! verify_checksum(checksum, cursor))
+//     return;
 
-  const float voltage = getval(cursor) / 100.0;
-  const float amps = getval(cursor) / 100.0;
-  const float ampHourRemaining = getval(cursor) / 1000.0;
-  const float ampHourTotalUsed = getval(cursor) / 100.00;
-  const float wattHourRemaining = getval(cursor) / 100.0;
-  const float runtimeSeconds = getval(cursor);
-  const float temperature = getval(cursor) - 100.0;
-  const float powerInWatts = getval(cursor) / 100.0;
-  const int relayStatus = getval(cursor);
-  const int direction = getval(cursor);
-  const int batteryLifeMinutes = getval(cursor);
-  const float batteryInternalOhms = getval(cursor) / 100.0;
-  ESP_LOGV("JunkTekKGF", "Recv %f %f %d %f %f %f", voltage, ampHourRemaining, direction, powerInWatts, amps, temperature);
-  if (voltage_sensor_)
-    this->voltage_sensor_->publish_state(voltage);
-  if (battery_level_sensor_ && this->battery_capacity_)
-    this->battery_level_sensor_->publish_state(ampHourRemaining * 100.0 / *this->battery_capacity_);
-  if (current_sensor_)
-    current_sensor_->publish_state(direction == 0 ? amps : - amps);
-  if (temperature_)
-    this->temperature_->publish_state(temperature);
+//   const float voltage = getval(cursor) / 100.0;
+//   const float amps = getval(cursor) / 100.0;
+//   const float ampHourRemaining = getval(cursor) / 1000.0;
+//   const float ampHourTotalUsed = getval(cursor) / 100.00;
+//   const float wattHourRemaining = getval(cursor) / 100.0;
+//   const float runtimeSeconds = getval(cursor);
+//   const float temperature = getval(cursor) - 100.0;
+//   const float powerInWatts = getval(cursor) / 100.0;
+//   const int relayStatus = getval(cursor);
+//   const int direction = getval(cursor);
+//   const int batteryLifeMinutes = getval(cursor);
+//   const float batteryInternalOhms = getval(cursor) / 100.0;
+//   ESP_LOGV("JunkTekKGF", "Recv %f %f %d %f %f %f", voltage, ampHourRemaining, direction, powerInWatts, amps, temperature);
+//   if (voltage_sensor_)
+//     this->voltage_sensor_->publish_state(voltage);
+//   if (battery_level_sensor_ && this->battery_capacity_)
+//     this->battery_level_sensor_->publish_state(ampHourRemaining * 100.0 / *this->battery_capacity_);
+//   if (current_sensor_)
+//     current_sensor_->publish_state(direction == 0 ? amps : - amps);
+//   if (temperature_)
+//     this->temperature_->publish_state(temperature);
 
-  this->last_stats_ = millis();
-}
+//   this->last_stats_ = millis();
+// }
 
-void JuncTekKGF::handle_line()
-{
-  const char* buffer = &this->line_buffer_[0];
-  try
-  {
-    if (buffer[0] != ':' || buffer[1] != 'r')
-      return;
-    if (strncmp(&buffer[2], "50=", 3) == 0)
-      handle_status(&buffer[5]);
-    else if (strncmp(&buffer[2], "51=", 3) == 0)
-      handle_settings(&buffer[5]);
-  }
-  catch(const std::exception& e)
-  {
-    ESP_LOGE("JunkTekKGF", "Error %s %s",e.what(), buffer);
-  }
+// void JuncTekKGF::handle_line()
+// {
+//   const char* buffer = &this->line_buffer_[0];
+//   try
+//   {
+//     if (buffer[0] != ':' || buffer[1] != 'r')
+//       return;
+//     if (strncmp(&buffer[2], "50=", 3) == 0)
+//       handle_status(&buffer[5]);
+//     else if (strncmp(&buffer[2], "51=", 3) == 0)
+//       handle_settings(&buffer[5]);
+//   }
+//   catch(const std::exception& e)
+//   {
+//     ESP_LOGE("JunkTekKGF", "Error %s %s",e.what(), buffer);
+//   }
 
-  return;
-}
+//   return;
+// }
 
-bool JuncTekKGF::readline()
-{
-  while (available()) {
-    const char readch = read();
-    if (readch > 0) {
-      switch (readch) {
-        case '\n': // Ignore new-lines
-          break;
-        case '\r': // Return on CR
-          this->line_pos_ = 0;  // Reset position index ready for next time
-          return true;
-        default:
-          if (this->line_pos_ < MAX_LINE_LEN - 1)
-          {
-            this->line_buffer_[this->line_pos_++] = readch;
-            this->line_buffer_[this->line_pos_] = 0;
-          }
-      }
-    }
-  }
-  return false;
-}
+// bool JuncTekKGF::readline()
+// {
+//   while (available()) {
+//     const char readch = read();
+//     if (readch > 0) {
+//       switch (readch) {
+//         case '\n': // Ignore new-lines
+//           break;
+//         case '\r': // Return on CR
+//           this->line_pos_ = 0;  // Reset position index ready for next time
+//           return true;
+//         default:
+//           if (this->line_pos_ < MAX_LINE_LEN - 1)
+//           {
+//             this->line_buffer_[this->line_pos_++] = readch;
+//             this->line_buffer_[this->line_pos_] = 0;
+//           }
+//       }
+//     }
+//   }
+//   return false;
+// }
 
 bool JuncTekKGF::verify_checksum(int checksum, const char* buffer)
 {
-  long total = 0;
-  try
-  {
-    while (true)
-    {
-      const int val = getval(buffer);
-      total += val;
-    }
-  }
-  catch (...)
-  {
+//   long total = 0;
 
-  }
-  const bool checksum_valid = (total % 255) + 1 == checksum;
-  ESP_LOGD("JunkTekKGF", "Recv checksum %d total %ld valid %d", checksum, total, checksum_valid);
-  return checksum_valid;
+//     while (true)
+//     {
+//       const int val = getval(buffer);
+//       ESP_LOGE("JunkTekKGF", "val = %d", val);
+//       total += val;
+//     }
+//   const bool checksum_valid = (total % 255) + 1 == checksum;
+//   ESP_LOGD("JunkTekKGF", "Recv checksum %d total %ld valid %d", checksum, total, checksum_valid);
+//   return checksum_valid;
 }
 
 void JuncTekKGF::loop()
@@ -190,11 +185,12 @@ void JuncTekKGF::loop()
     sprintf(buffer, ":R50=%d,2,1,\r\n", this->address_);
     write_str(buffer);
   }
+  ESP_LOGE("JunkTekKGF", "buffer = %s", buffer);
 
-  if (readline())
-  {
-    handle_line();
-  }
+//   if (readline())
+//   {
+//     handle_line();
+//   }
 }
 
 float JuncTekKGF::get_setup_priority() const
