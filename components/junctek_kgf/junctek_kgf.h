262ea7b6a97f5bcbbfd0ded0af15334f2abe77e9
Sebastian Muszynski <basti@linkt.de>
2023-04-15 10:01:49 +0200

    Apply clang-format

--- components/junctek_kgf/junctek_kgf.h
+++ components/junctek_kgf/junctek_kgf.h
@@ -6,12 +6,9 @@
 
 using namespace esphome;
 
-class JuncTekKGF
-  : public esphome::Component
-  , public uart::UARTDevice
-{
-public:
-  JuncTekKGF(unsigned address = 1, bool invert_current=false);
+class JuncTekKGF : public esphome::Component, public uart::UARTDevice {
+ public:
+  JuncTekKGF(unsigned address = 1, bool invert_current = false);
 
   void set_voltage_sensor(sensor::Sensor *voltage_sensor) { voltage_sensor_ = voltage_sensor; }
   void set_current_sensor(sensor::Sensor *current_sensor) { current_sensor_ = current_sensor; }
@@ -21,23 +18,23 @@ public:
   void dump_config() override;
   void loop() override;
 
-  float get_setup_priority() const;// override;
+  float get_setup_priority() const;  // override;
 
-protected:
+ protected:
   bool readline();
   void handle_line();
-  void handle_status(const char* buffer);
-  void handle_settings(const char* buffer);
+  void handle_status(const char *buffer);
+  void handle_settings(const char *buffer);
   void request_data(uint8_t data_id);
   void decode_data(std::vector<uint8_t> data);
-  bool verify_checksum(int checksum, const char* buffer);
+  bool verify_checksum(int checksum, const char *buffer);
 
   const unsigned address_;
 
-  sensor::Sensor* voltage_sensor_{nullptr};
-  sensor::Sensor* current_sensor_{nullptr};
-  sensor::Sensor* battery_level_sensor_{nullptr};
-  sensor::Sensor* temperature_{nullptr};
+  sensor::Sensor *voltage_sensor_{nullptr};
+  sensor::Sensor *current_sensor_{nullptr};
+  sensor::Sensor *battery_level_sensor_{nullptr};
+  sensor::Sensor *temperature_{nullptr};
 
   static constexpr int MAX_LINE_LEN = 120;
   std::array<char, MAX_LINE_LEN> line_buffer_;
